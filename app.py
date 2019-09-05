67ed37d73d7059846bb5bf808754315675eb6604
Paz <paz268@gmail.com>
2019-09-06 01:21:57 +0300

    use nutrition api

--- app.py
+++ app.py
@@ -6,54 +6,144 @@ from flask_assistant import Assistant, tell, ask, event
 from Recipe import Recipe
 from random import choice
 import json
-
+import requests
 import logging
+
 logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 
 FIRST_STEP = 0
 LIFESPAN = 100
 AWESOME_LIST = ["Awesome", "Great", "Wonderful", "What a"]
 
+
 class FlaskApp(Flask):
     def __init__(self, *args, **kwargs):
         super(FlaskApp, self).__init__(*args, **kwargs)
         self.mongo = database.Mongo()
+
 
 # initialize the flask app
 app = FlaskApp(__name__)
 assist = Assistant(app, route='/')
 ingredient_string_template = "Ok you need {amount} {measure} of {name}\n"
 
-@assist.action('get-recipe')
-def get_recipe(recipe, participants):
+
+@assist.action('choose-another')
+def choose_another():
     # print(app.mongo.get_ingredient(1))
-    recipe_doc = app.mongo.get_translated_recipe(recipe)
+    # recipe_doc = app.mongo.get_translated_recipe(recipe)
     # print(recipe_doc)
     # final = participants/recipe_doc['amount']
+    context = context_manager.get('make-food')
+    recipes = context.parameters.get('recipes')
+    current_recipe = int(context.parameters.get('current_recipe_index') + 1)
+    if len(recipes) <= current_recipe:
+        return tell("I don't have another recepies")
 
-    context_manager.add("make-food", lifespan=10)
+    context_manager.set("make-food", "current_recipe_index", current_recipe)
+    speech = "What a {choice} sounds like?".format(choice=recipes[current_recipe]['title'])
+    return ask(speech)
 
-    if not recipe_doc:
-        context_manager.clear_all()
-        return tell(recipe_not_exist(recipe))
-    recipeObj = Recipe(recipe_doc, participants)
-    context_manager.add("make-food", lifespan=LIFESPAN)
-    context_manager.set("make-food", "recipe", recipe)
+
+@assist.action('choose-current')
+def choose_current():
+    print('ppppppppppppppppppppppppppppppppppppppp')
+    context = context_manager.get('make-food')
+    recipes = context.parameters.get('recipes')
+    current_recipe = int(context.parameters.get('current_recipe_index'))
+    recipe_id = int(recipes[current_recipe]['id'])
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{id}/ingredientWidget.json".format(id=recipe_id)
+
+    headers = {
+        'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        'x-rapidapi-key': "0337527ffemsh1130c076beb971dp149cd5jsn1ec4dfe855bd"
+    }
+
+    ingredients_response = requests.request("GET", url, headers=headers)
+
     context_manager.set("make-food", "next_step", FIRST_STEP)
-    context_manager.set("make-food", "participants", participants)
 
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/324694/analyzedInstructions"
 
+    querystring = {"stepBreakdown": "false"}
+
+    headers = {
+        'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        'x-rapidapi-key': "0337527ffemsh1130c076beb971dp149cd5jsn1ec4dfe855bd"
+    }
+
+    steps_response = requests.request("GET", url, headers=headers, params=querystring)
+    context_manager.set("make-food", "recipe_steps", eval(steps_response.text))
     speech = "{random} choice! We found a recipe for {recipe}. The ingredients are: {ingredients}." \
              " Do you want to start making it?".format(
         random=choice(AWESOME_LIST),
-        recipe=recipe,
-        ingredients=get_ingredients_to_speech(recipeObj))
+        recipe=recipes[current_recipe]['title'],
+        ingredients=get_ingredients_to_speech(eval(ingredients_response.text)['ingredients']))
+    return ask(speech)
+
+
+@assist.action('get-recipe')
+def get_recipe(recipe, participants, diet=None, excludeIngredients=None, intolerances=None, type=None):
+    # print(app.mongo.get_ingredient(1))
+    recipe_doc = app.mongo.get_translated_recipe(recipe)
+    # final = participants/recipe_doc['amount']
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search"
+
+    querystring = {"diet": diet, "excludeIngredients": excludeIngredients, "intolerances": intolerances, "number": "10",
+                   "offset": "0", "type": type, "query": recipe}
+
+    headers = {
+        'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        'x-rapidapi-key': "0337527ffemsh1130c076beb971dp149cd5jsn1ec4dfe855bd"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    recipes = json.loads(r'' + response.text + '')['results']
+    context_manager.add("make-food", lifespan=10)
+
+    if len(recipes) == 0:
+        context_manager.clear_all()
+        return tell(recipe_not_exist(recipe))
+    # recipeObj = Recipe(recipe_doc, participants)
+    context_manager.add("make-food", lifespan=LIFESPAN)
+    context_manager.set("make-food", "recipes", recipes)
+    context_manager.set("make-food", "current_recipe_index", 0)
+    # context_manager.set("make-food", "recipe", recipe)
+    # context_manager.set("make-food", "next_step", FIRST_STEP)
+    context_manager.set("make-food", "participants", participants)
+
+    speech = "What a {choice} sounds like?".format(choice=recipes[0]['title'])
+    # speech = "{random} choice! We found a recipe for {recipe}. The ingredients are: {ingredients}." \
+    #         " Do you want to start making it?".format(
+    #    random=choice(AWESOME_LIST),
+    #    recipe=recipe,
+    #    ingredients=get_ingredients_to_speech(recipeObj))
     return ask(speech)
 
 
 @assist.action('get-recipe - yes')
+def choose():
+    return choose_current()
+
+
+@assist.action('get-recipe - no')
+def find_another():
+    return choose_another()
+
+@assist.action('choose-another - no')
+def find_another():
+    return choose_another()
+
+@assist.action('choose-another - yes')
+def choose():
+    print('asdf')
+    return choose_current()
+
+@assist.action('choose-current - yes')
 def first_step():
     return next_step()
+
 
 @assist.action('next-step')
 def next_step():
@@ -63,34 +153,39 @@ def next_step():
     except Exception:
         speech = "Hold your horses! please ask for a recipe first. Please try something like how to make lasagna for 2 people?"
         return tell(speech)
-    recipe_doc = app.mongo.get_recipe(context.parameters.get('recipe'))
-    if int(next_step) >= len(recipe_doc.get("steps")):
-        # no more steps
-        speech = "You finished the recipe! bonappetit"
-        context_manager.clear_all()
-        return tell(speech)
+    # recipe_doc = app.mongo.get_recipe(context.parameters.get('recipe'))
+    steps = context.get('recipe_steps')
+    print(steps)
+    #if int(next_step) >= len(recipe_doc.get("steps")):
+    #    # no more steps
+    #    speech = "You finished the recipe! bonappetit"
+    #    context_manager.clear_all()
+    #    return tell(speech)
 
-    speech = recipe_doc.get('steps')[int(next_step)].get('description')
-    context_manager.set('make-food', 'next_step', int(next_step) + 1)
-    return tell(speech)
+    #speech = recipe_doc.get('steps')[int(next_step)].get('description')
+    #context_manager.set('make-food', 'next_step', int(next_step) + 1)
+    #return tell(speech)
 
-def get_ingredients_to_speech(recipeObj):
-    last_ingredient = recipeObj.ingredients[-1]
+
+def get_ingredients_to_speech(ingredients):
+    last_ingredient = ingredients[-1]
     txt = ""
-    # print (recipeObj.ingredients)
-    for ingredient in recipeObj.ingredients:
-        if ingredient.name == last_ingredient.name:
-            txt = txt[0:-2] + " and {amount} {measure} of {name}".format(amount=ingredient.amount,
-                                                                         measure=ingredient.measure,
-                                                                         name=ingredient.name)
+
+    for ingredient in ingredients:
+        if ingredient['name'] == last_ingredient['name']:
+            txt = txt[0:-2] + " and {amount} {measure} of {name}".format(amount=ingredient['amount']['metric']['value'],
+                                                                         measure=ingredient['amount']['metric']['unit'],
+                                                                         name=ingredient['name'])
         else:
-            txt = txt + "{amount} {measure} of {name}, ".format(amount=ingredient.amount,
-                                                                measure=ingredient.measure,
-                                                                name=ingredient.name)
+            txt = txt + "{amount} {measure} of {name}, ".format(amount=ingredient['amount']['metric']['value'],
+                                                                measure=ingredient['amount']['metric']['unit'],
+                                                                name=ingredient['name'])
     return txt
+
 
 def recipe_not_exist(recipe):
     return "Could not find a recipe for {}. Please search for a different recipe?".format(recipe)
+
 
 # run the app
 if __name__ == '__main__':
